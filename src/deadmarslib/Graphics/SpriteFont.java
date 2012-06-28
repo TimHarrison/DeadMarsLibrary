dcc6b2fd697d4713e8653516b418f47cc37211b1
Daniel Cecil <cecil15620@gmail.com>
2012-06-28 05:05:46 -0700

    Modified and added javadoc comments.

--- src/deadmarslib/Graphics/SpriteFont.java
+++ src/deadmarslib/Graphics/SpriteFont.java
@@ -1,19 +1,42 @@
 package deadmarslib.Graphics;
 
+// <editor-fold defaultstate="collapsed" desc="Imports">
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.HashMap;
+// </editor-fold>
 
+/**
+ * DeadMarsLibrary SpriteFont class
+ * 
+ * @author Daniel Cecil
+ */
 public class SpriteFont {
     
     private HashMap<String, Integer> characterMap = new HashMap<>();
     private SpriteSheet characterSheet;
     
+    /**
+     * Constructor
+     * 
+     * @param cSheet {@link SpriteSheet} to use for the font.
+     * @param cMap Character {@link SpriteSheet} cell mappings.
+     */
     public SpriteFont(SpriteSheet cSheet, HashMap<String, Integer> cMap) {
         characterSheet = cSheet;
         characterMap = cMap;
     }
     
+    /**
+     * Draws a message to a graphics context using the {@link SpriteSheet} and character mappings.
+     * <p>
+     * If a character is not mapped, a rectangular box is drawn in the current graphics context color.
+     * 
+     * @param msg Message to draw.
+     * @param g Graphics context to draw to.
+     * @param x X position to start drawing.
+     * @param y Y position to start drawing.
+     */
     public void drawMessage(String msg, Graphics g, int x, int y) {
         int chrIndx = 0;
         for(char c : msg.toCharArray()) {
