d4c50f6fc3594519408f8f2aec97b66e604b3020
Daniel Cecil <cecil15620@gmail.com>
2012-06-28 04:45:03 -0700

    Added javadoc comments.

--- src/deadmarslib/Game/GameCamera.java
+++ src/deadmarslib/Game/GameCamera.java
@@ -15,65 +15,133 @@ public class GameCamera {
     int Y;
     int Width;
     int Height;
-//    int Scale;
     
+    /**
+     * Retrieve the X position of this {@link GameCamera}.
+     * 
+     * @return X position.
+     */
     public int getX() {
         return X;
     }
     
+    /**
+     * Set the X position of this {@link GameCamera}.
+     * 
+     * @param x new X position.
+     */
     public void setX(int x) {
         X = x;
     }
 
+    /**
+     * Retrieve the Y position of this {@link GameCamera}.
+     * 
+     * @return Y position.
+     */
     public int getY() {
         return Y;
     }
     
+    /**
+     * Set the Y position of this {@link GameCamera}.
+     * 
+     * @param y new Y position.
+     */
     public void setY(int y) {
         Y = y;
     }
     
+    /**
+     * Retrieve the width of this {@link GameCamera}.
+     * 
+     * @return width.
+     */
     public int getWidth() {
         return Width;
     }
     
+    /**
+     * Sets the width of this {@link GameCamera}.
+     * 
+     * @param w new width.
+     */
     public void setWidth(int w) {
         Width = w;
     }
     
+    /**
+     * Retrieve the height of this {@link GameCamera}.
+     * 
+     * @return height.
+     */
     public int getHeight() {
         return Height;
     }
     
+    /**
+     * Sets the height of this {@link GameCamera}.
+     * 
+     * @param w new height.
+     */
     public void setHeight(int h) {
         Height = h;
     }
     
-//    public int getScale() {
-//        return Scale;
-//    }
-    
-//    public void setScale(int s){
-//        Scale = s;
-//    }
-    
+    /**
+     * Default constructor.
+     * <p>
+     * Creates a size 0 {@link GameCamera} at position (0, 0).
+     */
     public GameCamera() {
         setCamera(0, 0, 0, 0);
     }
     
+    /**
+     * Basic Constructor.
+     * <p>
+     * Creates a size 0 {@link GameCamera} at position (x, y).
+     * 
+     * @param x
+     * @param y 
+     */
     public GameCamera(int x, int y) {
         setCamera(x, y, 0, 0);
     }
-	
+    
+    /**
+     * Versatile Constructor.
+     * <p>
+     * Creates a size (w, h) {@link GameCamera} at position (x, y).
+     * 
+     * @param x
+     * @param y
+     * @param w
+     * @param h 
+     */
     public GameCamera(int x, int y, int w, int h) {
         setCamera(x, y, w, h);
     }
     
+    /**
+     * Sets this {@link GameCamera}'s X and Y position.
+     * 
+     * @param x
+     * @param y 
+     */
     public final void setCamera(int x, int y) {
         X = x;
         Y = y;
     }
     
+    /**
+     * Sets this {@link GameCamera}'s X and Y position as well as width and height dimensions.
+     * 
+     * @param x
+     * @param y 
+     * @param w 
+     * @param h 
+     */
     public final void setCamera(int x, int y, int w, int h) {
         X = x;
         Y = y;
@@ -81,6 +149,10 @@ public class GameCamera {
         Height = h;
     }
     
+    /**
+     * Retrieves the rectangular area of this {@link GameCamera}.
+     * @return {@link Rectangle} of camera.
+     */
     public Rectangle getCamera() {
         return new Rectangle(X, Y, Width, Height);
     }
