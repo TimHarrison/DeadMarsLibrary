e3235fbdc4e9ae7cfb8b34dd0cbbdfef22209a3a
Daniel Cecil <cecil15620@gmail.com>
2015-08-26 17:15:22 -0700

    getRenderDetails for MessageBoxScreen
    Implemented most of the getRenderDetails method for MessageBoxScreen.

--- src/com/cecilectomy/dmge/Screens/SystemScreens/MessageBoxScreen.java
+++ src/com/cecilectomy/dmge/Screens/SystemScreens/MessageBoxScreen.java
@@ -1,15 +1,17 @@
 package com.cecilectomy.dmge.Screens.SystemScreens;
 
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.awt.FontMetrics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.cecilectomy.dmge.Core.GameInput;
-import com.cecilectomy.dmge.Rendering.Java2DRenderer;
-import com.cecilectomy.dmge.Rendering.Renderer;
+import com.cecilectomy.dmge.Rendering.RenderDetails;
 import com.cecilectomy.dmge.Screens.Screen;
 import com.cecilectomy.dmge.Utility.TimeSpan;
 
@@ -64,39 +66,101 @@ public class MessageBoxScreen extends Screen {
 			exitScreen();
 		}
 	}
+	
+	@Override
+	public List<RenderDetails> getRenderDetails() {
+		ArrayList<RenderDetails> details = new ArrayList<RenderDetails>();
+		
+		int posx, posy;
+		
+		Canvas c = new Canvas();
+		FontMetrics fm = c.getFontMetrics(mbFont);
+		int textWidth = fm.stringWidth(message);
+		int textHeight = fm.getHeight();
 
-	// TODO (Daniel): Implement getRenderDetails method for MessageBoxScreen.
-//	@Override
-//	public void render(Renderer renderer) {
-//		// TODO (Daniel): Use renderer with render details
-//		Graphics g = ((Java2DRenderer)renderer).getGraphics();
-//		
-//		g.setFont(mbFont);
-//		this.getScreenManager().fadeBackBufferToBlack(g,
-//				this.getTransitionAlpha() * 2 / 3);
-//
-//		int posx, posy;
-//		int textWidth = g.getFontMetrics().stringWidth(message);
-//		int textHeight = g.getFontMetrics().getHeight();
-//
-//		posx = renderer.getResolution().width / 2;
-//		posy = renderer.getResolution().height / 2;
-//
-//		final int hPad = 32;
-//		final int vPad = 16;
-//
-//		Rectangle br = new Rectangle((int) posx - textWidth / 2 - hPad,
-//				(int) posy - textHeight - vPad, (int) posx + textWidth / 2
-//						+ hPad, (int) posy + textHeight * 2 + textHeight / 2
-//						+ vPad);
-//
-//		g.setColor(Color.white);
+		posx = this.getScreenManager().getGame().getRenderer().getResolution().width / 2;
+		posy = this.getScreenManager().getGame().getRenderer().getResolution().height / 2;
+
+		final int hPad = 32;
+		final int vPad = 16;
+		
+		Rectangle rect = new Rectangle(
+			posx - textWidth / 2 - hPad,
+			posy - textHeight / 2 - vPad,
+			textWidth + hPad * 2,
+			textHeight * 4 + vPad * 2
+		);
+		
+		{
+			RenderDetails detail = new RenderDetails();
+			
+			detail.details.put("type", "Rectangle");
+			detail.details.put("rect", new Rectangle(
+					0,0,
+					this.getScreenManager().getGame().getRenderer().getResolution().width,
+					this.getScreenManager().getGame().getRenderer().getResolution().height
+			));
+			detail.details.put("style", "fill");
+			detail.details.put("color", Color.black);
+			detail.details.put("alpha", this.getTransitionAlpha() * 2 / 3);
+			details.add(detail);
+		}
+		
+		{
+			RenderDetails detail = new RenderDetails();
+			
+			detail.details.put("type", "Rectangle");
+			detail.details.put("rect", rect);
+			detail.details.put("style", "fill");
+			detail.details.put("color", Color.blue);
+			details.add(detail);
+		}
+		
+		// TODO (Daniel): Need to render textured background
 //		g.drawImage(gradientTexture, br.x, br.y, br.width, br.height, 0, 0,
-//				gradientTexture.getWidth(null),
-//				gradientTexture.getHeight(null), null);
-//		g.drawString(message, posx - textWidth / 2, posy);
-//		g.drawString(optionOK, posx - textWidth / 2, posy + textHeight);
-//		g.drawString(optionCancel, posx - textWidth / 2, posy + textHeight * 2);
-//	}
+//		gradientTexture.getWidth(null),
+//		gradientTexture.getHeight(null), null);
+		
+		{
+			RenderDetails detail = new RenderDetails();
+			
+			detail.details.put("type", "Text");
+			detail.details.put("text", this.message);
+			detail.details.put("color", Color.white);
+			detail.details.put("font", mbFont);
+			detail.details.put("x", posx - textWidth / 2);
+			detail.details.put("y", posy);
+			
+			details.add(detail);
+		}
+		
+		{
+			RenderDetails detail = new RenderDetails();
+			
+			detail.details.put("type", "Text");
+			detail.details.put("text", optionOK);
+			detail.details.put("color", Color.white);
+			detail.details.put("font", mbFont);
+			detail.details.put("x", posx - textWidth / 2);
+			detail.details.put("y", posy + textHeight * 2);
+			
+			details.add(detail);
+		}
+		
+		{
+			RenderDetails detail = new RenderDetails();
+			
+			detail.details.put("type", "Text");
+			detail.details.put("text", optionCancel);
+			detail.details.put("color", Color.white);
+			detail.details.put("font", mbFont);
+			detail.details.put("x", posx - textWidth / 2);
+			detail.details.put("y", posy + textHeight * 3);
+			
+			details.add(detail);
+		}
+		
+		return details;
+	}
 
 }
